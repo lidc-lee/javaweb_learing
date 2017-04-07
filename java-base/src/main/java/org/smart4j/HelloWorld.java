4493cf04a6fb19b9459e6c46c0c17712dd50ec77
AA-ldc <1499117534@qq.com>
2017-04-07 09:46:06 +0800

    first commit

---
+++ java-base/src/main/java/org/smart4j/HelloWorld.java
@@ -0,0 +1,10 @@
+package org.smart4j;
+
+/**
+ * Created by LEE on 2017/4/07.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
